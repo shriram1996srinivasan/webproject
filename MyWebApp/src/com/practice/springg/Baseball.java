949927805f80edadac9d842fca500ce8648f9d0a
shriram1996srinivasan <60841556+shriram1996srinivasan@users.noreply.github.com>
2022-12-18 19:36:48 +0530

    commit to master

--- MyWebApp/src/com/practice/springg/Baseball.java
+++ MyWebApp/src/com/practice/springg/Baseball.java
@@ -1,5 +1,7 @@
 package com.practice.springg;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Baseball implements Coach{
 
 	private FortumeService fortune;
@@ -9,6 +11,11 @@ public class Baseball implements Coach{
 	}
 	
 	
+	public Baseball() {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public void show1() {
 		System.out.println("Hello Baseball");
 	}
@@ -18,6 +25,11 @@ public class Baseball implements Coach{
 	public void getFor() {
 		fortune.showFortune();
 		
+	}
+	public void showNum() {
+		ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext("com/practice/springg/applicationContext.xml");
+		CricketCoach coach1 = cpx.getBean("myCcoach",CricketCoach.class);
+		System.out.println(coach1.getNum());
 	}
 	
 }
--- MyWebApp/src/com/practice/springg/Runner.java
+++ MyWebApp/src/com/practice/springg/Runner.java
@@ -6,9 +6,17 @@ public class Runner {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext cpx = new ClassPathXmlApplicationContext("com/practice/springg/applicationContext.xml");
-		Coach coach = cpx.getBean("mycoach",Coach.class);
+		CricketCoach coach = cpx.getBean("myCcoach",CricketCoach.class);
+		coach.num=3;
+		System.out.println(coach.getNum());
 		coach.show1();
 		coach.getFor();
+		cpx.close();
+		cpx.refresh();
+		
+		CricketCoach coach1 = cpx.getBean("myCcoach",CricketCoach.class);
+		System.out.println(coach1.getNum());
+		cpx.close();
 	}
 	
 	
